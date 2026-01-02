a6aafb1eb307d036649393b78329a4de1339efd8
jhzong <jhzong05@gmail.com>
2026-01-02 17:10:21 +0900

    reslist-DB연결~페이징

--- rebornPjt/restaurants/urls.py
+++ rebornPjt/restaurants/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'restaurants'
 urlpatterns = [
     path('reslist/', views.reslist, name='reslist'),
+    path('resview/', views.resview, name='resview'),
 ]
--- rebornPjt/restaurants/views.py
+++ rebornPjt/restaurants/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from restaurants.models import Restaurant
+from django.core.paginator import Paginator
 
 def reslist(request):
-    
-    return render(request,'restaurants/reslist.html')
+    qs = Restaurant.objects.all()
+    page=int(request.GET.get('page',1))# 없으면 default=1
+    paginator=Paginator(qs,20)#20개씩 자르기
+    list_qs=paginator.get_page(page)
+
+    context={'list':list_qs,'page':page}
+    return render(request,'restaurants/reslist.html',context)
+
+def resview(request):
+    return render(request,'restaurants/resview.html')
